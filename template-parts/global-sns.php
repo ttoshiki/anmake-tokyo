4decf619091dc6f7047fdbb379e5e2e2b2583bcf
t-tamaru <t-tamaru@sakura.ad.jp>
2020-10-02 16:47:50 +0900

    SNSURL変更取り入れ

--- template-parts/global-sns.php
+++ template-parts/global-sns.php
@@ -1,5 +1,5 @@
 <div class="globalSns">
-  <a href="" class="globalSnsLink"><?php echo file_get_contents(get_template_directory() . '/assets/img/icons/icon_twitter.svg'); ?></a>
-  <a href="" class="globalSnsLink"><?php echo file_get_contents(get_template_directory() . '/assets/img/icons/icon_instagram.svg'); ?></a>
-  <a href="" class="globalSnsLink"><?php echo file_get_contents(get_template_directory() . '/assets/img/icons/icon_fb.svg'); ?></a>
+  <a href="https://twitter.com/BBRYO0822" class="globalSnsLink"><?php echo file_get_contents(get_template_directory() . '/assets/img/icons/icon_twitter.svg'); ?></a>
+  <a href="https://www.instagram.com/anmaketokyo/" class="globalSnsLink"><?php echo file_get_contents(get_template_directory() . '/assets/img/icons/icon_instagram.svg'); ?></a>
+  <a href="https://www.facebook.com/profile.php?id=100024518706679" class="globalSnsLink"><?php echo file_get_contents(get_template_directory() . '/assets/img/icons/icon_fb.svg'); ?></a>
 </div>
